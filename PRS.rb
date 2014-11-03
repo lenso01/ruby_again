a5579b97afb1ad5aa40396eb374af00635c49d86
boss <lll1930Az>
2014-11-03 19:14:16 +0200

    update

--- PRS.rb
+++ PRS.rb
@@ -1,53 +1,152 @@
-class PRS
+class Hand
 
- attr_accessor :computer
+  include Comparable
+end
 
-  @@computer_selection = ["p","r","s"]
+
+
+
+class Player
+  attr_accessor :choice
+  attr_reader :name
+  def initialize(n)
+    @name = n
+  end
+
+  def to_s
+    "#{name} currently has a choice of #{choice}"
+  end
+end
+
+class Human < Player
+  def pick_hand
+    begin
+      puts ""
+      puts "Pick one: (p,r,s)"
+      c = gets.chomp.downcase
+    end until Game::CHOICES.keys.include?(c)
+
+    self.hand = Hand.new(c)
+  end
+
+end
+
+class Computer < Player
+    def pick_hand
+      self.choice = Game::CHOICES.keys.sample
+    end
+end
+
+#Hand
+
+
+class Game
+  CHOICES = {'p' => 'PAPER', 'r' => "Rock", 's' => 'Scissors'}
+
+  attr_reader :player, :computer
 
   def initialize
-    @computer = []
-    @option = []
-
+    @player = Human.new('Bob')
+    @computer = Computer.new('r2d2')
   end
 
-  def welcome
-    puts "Welcome to our Paper Scissor Rock Game. Good Luck"
-    puts "Papers, Scissor or Rock. Choose one "
 
 
+  def play
+
+    computer.pick_hand
+    player.pick_hand
+    puts player
+    puts computer
+    compare_hands
   end
 
-  def user_selection
-
-    @option << gets.chomp.downcase
-
-  end
-
-  def computer_turn
-    @computer << @@computer_selection.sample.to_s.downcase
-
-  end
-
-  def compare
-    if @option == @computer
-      puts "It is a tie"
-    elsif (@option = "p" && @computer ="r") || (@option = "s" && @computer ="p") || (@option = "r" && @computer ="s")
-      puts "You won"
-    elsif (@computer = "p" && @option ="r") || (@computer = "s" && @option ="p") || (@computer = "r" && @option ="s")
-
-    end
-
-  end
 
 
 end
 
 
-game = PRS.new
-puts game.welcome
-puts game.user_selection
-puts game.computer_turn
-game.computer
-game.compare
+
+game = Game.new.play
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class PRS
+#
+#  attr_accessor :computer
+#
+#   @@computer_selection = ["p","r","s"]
+#
+#   def initialize
+#     @computer = []
+#     @option = []
+#
+#   end
+#
+#   def welcome
+#     puts "Welcome to our Paper Scissor Rock Game. Good Luck"
+#     puts "Papers, Scissor or Rock. Choose one "
+#
+#
+#   end
+#
+#   def user_selection
+#
+#     @option << gets.chomp.downcase
+#
+#   end
+#
+#   def computer_turn
+#     @computer << @@computer_selection.sample.to_s.downcase
+#
+#   end
+#
+#   def compare
+#     if @option == @computer
+#       puts "It is a tie"
+#     elsif (@option = "p" && @computer ="r") || (@option = "s" && @computer ="p") || (@option = "r" && @computer ="s")
+#       puts "You won"
+#     elsif (@computer = "p" && @option ="r") || (@computer = "s" && @option ="p") || (@computer = "r" && @option ="s")
+#
+#     end
+#
+#   end
+#
+#
+# end
+#
+#
+# game = PRS.new
+# puts game.welcome
+# puts game.user_selection
+# puts game.computer_turn
+# game.computer
+# game.compare
 
 
--- prs_test.rb
+++ prs_test.rb
@@ -1,33 +1,41 @@
-class Player
+class Players
+
   attr_reader :name
   attr_accessor :choice
 
+
   def initialize(n)
-    @name =n
+    @name = n
   end
 
   def to_s
-     "#{name} currently has a choice of #{choice}!"
+    "#{name} choice is #{choice}"
   end
 
 end
 
-class Human < Player
+class Human < Players
+
   def pick_hand
     begin
-      puts "Pick one : (p,r,s):"
+      puts "Pick P,R,S"
       c = gets.chomp.downcase
 
     end until Game::CHOICES.keys.include?(c)
-    self.choice = c
+        self.choice = c
+
   end
 
 end
 
-class Computer < Player
-   def pick_hand
-     self.choice = Game::CHOICES.keys.sample
-   end
+class Computer < Players
+
+  def pick_hand
+
+  self.choice = Game::CHOICES.keys.sample
+
+  end
+
 
 end
 
@@ -35,24 +43,175 @@ end
 
 class Game
 
-  CHOICES = {'p' => 'Paper', 'r' => 'Rock', 's' => 'Scissors'}
+  CHOICES = {'p' => "Paper", 'r' => "Rock", 's' => 'Scissors'}
+
   attr_reader :player, :computer
 
   def initialize
-    @player = Human.new("Bob")
-    @computer = Computer.new("R2D2")
-
-
+    @player = Human.new('Ahmet')
+    @computer = Computer.new('Robotx')
   end
 
-  def play
-     player.pick_hand
-     computer.pick_hand
-    #compare_hands
-
+  def start
+    player.pick_hand
+    computer.pick_hand
+    puts player
+    puts computer
+    result
+    compare_hands
+    start
   end
 
+  def compare_hands
+    if player.choice == computer.choice
+      puts "It is a tie"
+    elsif (player.choice == 'p' && computer.choice == 'r') || (player.choice == 's' && computer.choice == 'p') || (player.choice == 'r' && computer.choice == 's')
+      puts "Player wins "
+    else
+      puts "Computer Wins"
+
+    end
+  end
+
+  def result
+    case
+      when player.choice == 'p' && computer.choice == 'r'
+        puts "Paper Wraps Rock"
+      when player.choice == 'r' && computer.choice == 's'
+        puts "Rock beats Scissor"
+      when player.choice == 's' && computer.choice == 'p'
+        puts "Scissor cuts Paper"
+      when computer.choice == 'p' && player.choice == 'r'
+        puts "Paper Wraps Rock"
+      when computer.choice == 'r' && player.choice == 's'
+        puts "Rock beats Scissor"
+      when computer.choice == 's' && player.choice == 'p'
+        puts "Scissor cuts Paper"
+    end
+  end
 
 end
 
-Game.new.play
+
+Game.new.start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Players
+#   attr_reader :name
+#   attr_accessor :choice
+#
+#   def initialize(n)
+#     @name = n
+#   end
+#
+#   def to_s
+#     "This is #{name} has choice of #{choice}!"
+#   end
+#
+#   def pick_hand
+#     begin
+#       puts "Pick one: (p,r,s)"
+#       c = gets.chomp.downcase
+#     end until Game::CHOICES.keys.include?(c)
+#     self.choice = c
+#
+#   end
+#
+#   def computer_pick_hand
+#     self.choice = Game::CHOICES.keys.sample
+#   end
+#
+# end
+#
+# class Game
+#   attr_accessor :choice, :player, :computer
+#
+#   CHOICES = {'p' => 'PAPER', 'r' => 'Rock', 's' => 'Scissors'}
+#
+#   def initialize
+#     @player = Players.new('Ahmet')
+#     @computer = Players.new('RX200')
+#   end
+#
+#
+#   def start
+#     player.pick_hand
+#     computer.computer_pick_hand
+#     puts player
+#     puts computer
+#
+#
+#   end
+#
+# end
+#
+# Game.new.start
+
+
+# class Game
+#   CHOICES = {'p' => 'PAPER', 'r' => 'Rock', 's' => 'Scissors'}
+#   attr_accessor :player
+#   def initialize
+#     @player = ('Ahmet')
+#   end
+#
+#   def to_s
+#     "this is my #{player}"
+#   end
+#
+#   def pick_hand
+#     begin
+#       puts ""
+#       puts "Pick one: (p,r,s)"
+#       choice = gets.chomp.downcase
+#     end until CHOICES.keys.include?(choice)
+#
+#   end
+#
+#   def show_choices
+#     choice
+#   end
+#
+#
+# end
+#
+# class Engine
+#   attr_accessor :player, :computer, :choice
+#
+#   def initialize
+#     @player = Game.new
+#     @computer = Game.new
+#
+#
+#   end
+#
+#   def begin
+#     player.pick_hand
+#     puts choice
+#     computer.pick_hand
+#   end
+#
+# end
+#
+# engine = Engine.new
+# engine.begin
